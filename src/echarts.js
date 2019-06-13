c07e096ada12f868877fbbd3766f0a89a4459403
akira-cn <akira.cn@gmail.com>
2019-06-13 15:44:09 +0800

    :bug:fixed chart resize bug

--- src/echarts.js
+++ src/echarts.js
@@ -4,11 +4,6 @@ export default function install({use, utils, registerNodeType, BaseSprite}) {
       const container = document.createElement('div');
       container.style.width = `${w}px`;
       container.style.height = `${h}px`;
-      container.style.position = 'absolute';
-      container.style.left = '-10000px';
-      container.style.overflow = 'hidden';
-      // container.style.display = 'none';
-      document.body.appendChild(container);
       this.chart = echarts.init(container);
       this.chartContainer = container;
 
@@ -29,16 +24,13 @@ export default function install({use, utils, registerNodeType, BaseSprite}) {
 
       if(!this.chart) this.createContext(w, h);
 
-      const chartDiv = this.chartContainer;
-      const {clientWidth, clientHeight} = chartDiv;
-      // console.log(clientWidth, clientHeight);
-      if(w !== clientWidth || h !== clientHeight) {
-        // console.log('resize');
-        chartDiv.style.width = `${w}px`;
-        chartDiv.style.height = `${h}px`;
+      const canvas = this.chart.getRenderedCanvas();
+
+      if(w !== canvas.width || h !== canvas.height) {
+        this.chartContainer.style.width = `${w}px`;
+        this.chartContainer.style.height = `${h}px`;
         this.chart.resize();
       }
-      const canvas = this.chart.getRenderedCanvas();
       drawingContext.drawImage(canvas, 0, 0, w, h);
     }
   }
