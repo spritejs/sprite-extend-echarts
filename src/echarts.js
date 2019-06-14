e3c8fd046d7e700a60664e395c796ff4d239c553
akira-cn <akira.cn@gmail.com>
2019-06-14 15:22:03 +0800

    support events

--- src/echarts.js
+++ src/echarts.js
@@ -1,16 +1,66 @@
 export default function install({use, utils, registerNodeType, BaseSprite}) {
   class EChart extends BaseSprite {
     createContext(w, h) {
-      const container = document.createElement('div');
-      container.style.width = `${w}px`;
-      container.style.height = `${h}px`;
-      this.chart = echarts.init(container);
-      this.chartContainer = container;
+      const container = document.createElement('canvas');
+      const layer = this.layer;
 
-      this.chart.setOption(this.option);
-      this.chart.on('rendered', () => {
+      const pixelRatioX = layer.resolution[0] / layer.viewport[0];
+      const pixelRatioY = layer.resolution[1] / layer.viewport[1];
+      container.width = w / pixelRatioX;
+      container.height = h / pixelRatioY;
+
+      this.pixelRatio = [pixelRatioX, pixelRatioY];
+
+      const chart = echarts.init(container);
+
+      chart.setOption(this.option);
+      chart.on('rendered', () => {
         this.forceUpdate(true);
       });
+
+      const events = [
+        'click', 'dblclick', 'mouseout',
+        'mouseup', 'mousedown', 'mousemove',
+        'touchstart', 'touchend', 'touchmove',
+      ];
+
+      const handler = (e) => {
+        const originalEvent = e.originalEvent;
+        const anchor = this.attr('anchor');
+        const cz = this.clientSize;
+        const offsetX = (e.offsetX + anchor[0] * cz[0]) / pixelRatioX;
+        const offsetY = (e.offsetY + anchor[1] * cz[1]) / pixelRatioY;
+        let type = originalEvent.type;
+        if(type === 'mouseleave') type = 'mouseout';
+        const newEvent = new MouseEvent(originalEvent.type, {
+          screenX: offsetX,
+          screenY: offsetY,
+          clientX: offsetX,
+          clientY: offsetY,
+          bubbles: originalEvent.bubbles,
+          button: originalEvent.button,
+          buttons: originalEvent.buttons,
+          cancelBubble: originalEvent.cancelBubble,
+          cancelable: originalEvent.cancelable,
+          currentTarget: originalEvent.currentTarget,
+          fromElement: originalEvent.fromElement,
+          relatedTarget: originalEvent.relatedTarget,
+          returnValue: originalEvent.returnValue,
+          srcElement: originalEvent.srcElement,
+          target: originalEvent.target,
+          toElement: originalEvent.toElement,
+          view: originalEvent.view,
+          which: originalEvent.witch,
+        });
+
+        this.chart.getDom().dispatchEvent(newEvent);
+      };
+
+      events.forEach((type) => {
+        this.on(type, handler);
+      });
+
+      this.chart = chart;
     }
 
     setOption(option) {
@@ -24,12 +74,12 @@ export default function install({use, utils, registerNodeType, BaseSprite}) {
 
       if(!this.chart) this.createContext(w, h);
 
-      const canvas = this.chart.getRenderedCanvas();
+      const canvas = this.chart.getRenderedCanvas({pixelRatio: 2});
 
       if(w !== canvas.width || h !== canvas.height) {
-        this.chartContainer.style.width = `${w}px`;
-        this.chartContainer.style.height = `${h}px`;
-        this.chart.resize();
+        const width = w / this.pixelRatio[0];
+        const height = h / this.pixelRatio[1];
+        this.chart.resize({width, height});
       }
       drawingContext.drawImage(canvas, 0, 0, w, h);
     }
